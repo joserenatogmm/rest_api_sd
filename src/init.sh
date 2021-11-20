f3de7ebca7a8e9fe8d96008fa50ef56ae3af33b2
José Renato Garcia Martinez Martins <jose.martins@tray.net.br>
2021-11-20 15:40:23 -0300

    copy .env

--- src/init.sh
+++ src/init.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
+# Copia o .env
+cp .env.example .env
+
+# Instala as Dependencias
 composer install
+
+# Set-up laravel
 php artisan key:generate
 php artisan migrate:fresh
 php artisan db:seed --class=Alunos
